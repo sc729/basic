3894275ac31eee5d242261ebce24bb6f42be0483
suhJh <sjs0701@me.com>
2016-09-07 18:37:45 +0900

    gulp로 빌드 자동화 셋팅

---
+++ gulpfile.js
@@ -0,0 +1,79 @@
+const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps')
+const webserver = require('gulp-webserver');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const minifyhtml = require('gulp-minify-html');
+const sass = require('gulp-sass');
+const livereload = require('gulp-livereload');
+const babel = require('gulp-babel');
+
+
+var src = 'src/main/resources';
+var dist = 'target/classes';
+
+var paths = {
+	js: src + '/static/myFirstReactComponent/*.js',
+	scss: src + '/scss/*.scss',
+	html: src + '/**/*.html'
+};
+
+//웹서버를 localhost:8000 로 실행한다.
+gulp.task('server', function () {
+	return gulp.src(dist + '/static/')
+		.pipe(webserver());
+});
+
+// 자바스크립트 파일을 하나로 합치고 압축한다.
+gulp.task('combine-js', function () {
+	return gulp.src(src + '/static/bundle.js')
+		.pipe(concat('script.js'))
+		.pipe(uglify()) //-> 왠에러..?
+		.pipe(gulp.dest(dist + '/'));
+});
+
+// sass 파일을 css 로 컴파일한다.
+gulp.task('compile-sass', function () {
+	return gulp.src(paths.scss)
+		.pipe(sass())
+		.pipe(gulp.dest(dist + '/css'));
+});
+
+// HTML 파일을 압축한다.
+gulp.task('compress-html', function () {
+	return gulp.src(paths.html)
+		.pipe(minifyhtml())
+		.pipe(gulp.dest(dist + '/'));
+});
+
+// 파일 변경 감지 및 브라우저 재시작
+gulp.task('watch', function () {
+	livereload.listen();
+	gulp.watch(paths.js, ['combine-js']);
+	gulp.watch(paths.scss, ['compile-sass']);
+	gulp.watch(paths.html, ['compress-html']);
+	gulp.watch(dist + '/**').on('change', livereload.changed);
+});
+
+gulp.task('babelify', () => {
+    return gulp.src('src/main/resources/static/myFirstReactComponent/myOwn.js')
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ['es2015', 'react']
+        }))
+        .pipe(concat('allinone.js'))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('src/main/resources/static'));
+});
+
+
+
+//기본 task 설정
+gulp.task('default', [
+	'server', 
+	'combine-js', 
+	'compile-sass', 
+	'compress-html', 
+	'watch' 
+	]);
--- src/main/resources/static/bundle.js
+++ src/main/resources/static/bundle.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-console.error("Error: Cannot find module 'C:\\sts-bundle\\workspace\\basic\\src\\main\\resources\\static\\myFirstReactComponent\\myOwn.jsx' from 'C:\\sts-bundle\\workspace\\basic'");
-=======
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function (process){
 'use strict';
@@ -20896,4 +20893,3 @@ var InputField = _react2.default.createClass({
 _reactDom2.default.render(_react2.default.createElement(Something, { url: '/trs/search/findByTrNameIgnoreCaseContaining', title: 'new Tr search' }), document.getElementById('content'));
 
 },{"react":171,"react-dom":28}]},{},[172]);
->>>>>>> origin/master
